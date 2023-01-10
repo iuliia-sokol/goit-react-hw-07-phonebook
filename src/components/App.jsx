d5ca7e4000f2a1f3b184da169c7319cf9740f8c0
Iuliia Sokol <avare779@gmail.com>
2023-01-10 15:42:56 +0200

    Loader connected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
+import { useSelector } from 'react-redux';
 import { Container, MainHeader, SubHeader } from './App.styled';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
+import { Loader } from './Loader/Loader';
 
 export const App = () => {
+  const loader = useSelector(state => state.contacts.isLoading);
   return (
     <Container>
+      {loader && <Loader />}
       <MainHeader>Phonebook</MainHeader>
       <ContactForm />
       <SubHeader>Contacts</SubHeader>
