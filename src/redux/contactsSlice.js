4c7382e27cf79237b6d2218b2b401a7958740095
Iuliia Sokol <avare779@gmail.com>
2023-01-10 17:08:28 +0200

    useless builder mentioning removed

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -13,53 +13,50 @@ export const contactsSlice = createSlice({
   },
 
   extraReducers: builder => {
-    builder.addCase(fetchContacts.pending, state => {
-      state.isLoading = true;
-    });
-    builder.addCase(fetchContacts.fulfilled, (state, { payload }) => {
-      state.items = payload;
-      state.isLoading = false;
-      state.error = null;
-    });
-
-    builder.addCase(fetchContacts.rejected, (state, { payload }) => {
-      state.error = payload;
-      state.isLoading = false;
-    });
-
-    builder.addCase(addContact.pending, state => {
-      state.isLoading = true;
-    });
-    builder.addCase(addContact.fulfilled, (state, { payload }) => {
-      state.items.push(payload);
-      state.isLoading = false;
-      state.error = null;
-      Notiflix.Notify.success(
-        `${payload.name} was successfully added to your contacts`,
-        notifySettings
-      );
-    });
-
-    builder.addCase(addContact.rejected, (state, { payload }) => {
-      state.error = payload;
-      state.isLoading = false;
-    });
-    builder.addCase(deleteContact.pending, state => {
-      state.isLoading = true;
-    });
-    builder.addCase(deleteContact.fulfilled, (state, { payload }) => {
-      state.items = state.items.filter(contact => contact.id !== payload);
-      state.isLoading = false;
-      state.error = null;
-      Notiflix.Notify.info(
-        `${payload.name} was successfully deleted from your contacts`,
-        notifySettings
-      );
-    });
-
-    builder.addCase(deleteContact.rejected, (state, { payload }) => {
-      state.error = payload;
-      state.isLoading = false;
-    });
+    builder
+      .addCase(fetchContacts.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(fetchContacts.fulfilled, (state, { payload }) => {
+        state.items = payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(fetchContacts.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(addContact.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(addContact.fulfilled, (state, { payload }) => {
+        state.items.push(payload);
+        state.isLoading = false;
+        state.error = null;
+        Notiflix.Notify.success(
+          `${payload.name} was successfully added to your contacts`,
+          notifySettings
+        );
+      })
+      .addCase(addContact.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(deleteContact.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(deleteContact.fulfilled, (state, { payload }) => {
+        state.items = state.items.filter(contact => contact.id !== payload);
+        state.isLoading = false;
+        state.error = null;
+        Notiflix.Notify.info(
+          `${payload.name} was successfully deleted from your contacts`,
+          notifySettings
+        );
+      })
+      .addCase(deleteContact.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      });
   },
 });
